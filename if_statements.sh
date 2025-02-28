8564003156deab1c084cf9651d4fd31db9c795da
ChristopherMulwa <157383238+ChristopherMulwa@users.noreply.github.com>
2025-02-28 12:33:29 +0300

    Created an If statement

---
+++ if_statements.sh
@@ -0,0 +1,13 @@
+#1/bin/bash
+
+# This script demonstrates the use of if statements
+echo "Enter a number: "
+read number
+
+if [ $number -gt 10 ]; then
+    echo "The number is greater than 10."
+elif [ $number -eq 10 ]; then
+    echo "The number is equal to 10."
+else
+    echo "The number is less than 10."
+fi
