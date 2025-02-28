6d1d9b4b0219e18f54b955adee91ab23b234e1d8
ChristopherMulwa <157383238+ChristopherMulwa@users.noreply.github.com>
2025-02-28 13:17:14 +0300

    a simple loop

---
+++ loops.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+for i in {1..5}
+do
+    echo "Welcome $i"
+done
+# In this script, we used a for loop to print "Welcome" five times. The for loop iterates over the numbers 1 to 5, and for each iteration, it prints "Welcome" followed by the current number.
+
